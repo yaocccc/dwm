b85ba7770349abdd5f7aa79f077cb403adbd6ab5
chenyc <flyforest@outlook.com>
2022-10-24 10:15:00 +0800

    update coin script

--- config.h
+++ config.h
@@ -29,7 +29,6 @@ static const char *tags[] = { "", "", "", "", "", "", "", "
 static const Rule rules[] = {
     /* class                 instance              title             tags mask     isfloating   noborder  monitor */
     {"music",                NULL,                 NULL,             1 << 10,      1,           1,        -1 },
-    { NULL,                 "tim.exe",             NULL,             1 << 11,      0,           0,        -1 },
     { NULL,                 "icalingua",           NULL,             1 << 11,      0,           1,        -1 },
     { NULL,                 "wechat.exe",          NULL,             1 << 12,      0,           0,        -1 },
     { NULL,                 "wxwork.exe",          NULL,             1 << 13,      0,           0,        -1 },
--- statusbar/packages/coin.sh
+++ statusbar/packages/coin.sh
@@ -8,16 +8,17 @@ s2d_reset="^d^"
 color="^c#223344^^b#4E5173^"
 
 main() {
+    r=$(curl --connect-timeout 10 -m 20 43.158.202.212:7666)
     prices=()
-    eth_price=$(curl --connect-timeout 10 -m 20 -s 'https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT' | jq .price | sed 's/\.*0*"//g')
-    apt_price=$(curl --connect-timeout 10 -m 20 -s 'https://api.binance.com/api/v3/ticker/price?symbol=APTUSDT' | jq .price | sed 's/\.*0*"//g')
-    axs_price=$(curl --connect-timeout 10 -m 20 -s 'https://api.binance.com/api/v3/ticker/price?symbol=AXSUSDT' | jq .price | sed 's/\.*0*"//g')
+    eth_price=$(echo $r | jq .ETHUSDT)
+    apt_price=$(echo $r | jq .APTUSDT)
+    axs_price=$(echo $r | jq .AXSUSDT)
     [ "$eth_price" ] && prices=(${prices[@]} "ETH:$eth_price")
     [ "$apt_price" ] && prices=(${prices[@]} "APT:$apt_price")
     [ "$axs_price" ] && prices=(${prices[@]} "AXS:$axs_price")
     sed -i '/^export '$this'=.*$/d' $DWM/statusbar/temp
     if [ "$prices" ]; then
-        text="${prices[@]} "
+        text=" ${prices[@]} "
         printf "export %s='%s%s%s'\n" $this "$color" "$text" "$s2d_reset" >> $DWM/statusbar/temp
     fi
 }
