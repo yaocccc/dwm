10c55fedb8ae624a89c297b3393b7d549cc96e22
chenyc <flyforest@outlook.com>
2022-12-06 10:48:25 +0800

    remove coin script

--- statusbar/statusbar.sh
+++ statusbar/statusbar.sh
@@ -10,6 +10,7 @@ update() {
     update $*                                                            # 递归调用
 }
 
+# 处理状态栏点击
 click() {
     [ ! "$1" ] && return                                                 # 未传递参数时 结束
     bash $DWM/statusbar/packages/$1.sh click $2                          # 执行指定模块脚本
@@ -19,15 +20,14 @@ click() {
 
 # 更新状态栏
 refresh() {
-    _icons=''; _coin=''; _cpu=''; _mem=''; _date=''; _vol=''; _bat='';   # 重置所有模块的状态为空
+    _icons=''; _cpu=''; _mem=''; _date=''; _vol=''; _bat='';             # 重置所有模块的状态为空
     source $DWM/statusbar/temp                                           # 从 temp 文件中读取模块的状态
-    xsetroot -name "$_icons$_coin$_cpu$_mem$_date$_vol$_bat"             # 更新状态栏
+    xsetroot -name "$_icons$_cpu$_mem$_date$_vol$_bat"                   # 更新状态栏
 }
 
 # 启动定时更新状态栏 不用的package有不同的刷新周期 注意不要重复启动该func
 cron() {
     while true; do update cpu mem;        sleep 20;  done &              # 每隔20s更新cpu 内存
-    while true; do update coin;           sleep 20;  done &              # 每隔20s更新币价
     while true; do update vol icons;      sleep 60;  done &              # 每隔60s更新音量 图标
     while true; do update bat;            sleep 300; done &              # 每隔300s更新电池
     while true; do update date; refresh;  sleep 5;   done &              # 每隔5s更新时间并更新状态栏
@@ -37,7 +37,6 @@ case $1 in
     cron) cron ;;
     refresh) refresh ;;
     update) shift 1; update $*; refresh ;;
-    updateall) update icons coin cpu mem date vol bat; refresh ;;
-    # 接收clickstatusbar传递过来的信号 $1: 模块名  $2: 按键(L|M|R)
-    *) click $1 $2 ;;
+    updateall) update icons cpu mem date vol bat; refresh ;;
+    *) click $1 $2 ;; # 接收clickstatusbar传递过来的信号 $1: 模块名  $2: 按键(L|M|R)
 esac
