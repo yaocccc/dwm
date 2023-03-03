7f9896ec5b86376900ca9d5a24c388b2d5301cca
BingCoke <81607010+BingCoke@users.noreply.github.com>
2023-03-03 18:06:23 +0800

    acpi正常工作就取消执行upower
    如果bat_text有值说明并且大于0 说明acpi能够正常工作 返回即可

--- DEF/statusbar/packages/bat.sh
+++ DEF/statusbar/packages/bat.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # 电池电量
 # 需要安装acpi或者upower
+# 
 
 tempfile=$(cd $(dirname $0);cd ..;pwd)/temp
 
@@ -13,17 +14,18 @@ get_by_acpi() {
     [ ! "$(command -v acpi)" ] && echo command not found: acpi && return
     bat_text=$(acpi -b | sed '2,$d' | awk '{print $4}' | grep -Eo "[0-9]+")
     [ ! "$bat_text" ] && bat_text=$(acpi -b | sed '2,$d' | awk -F'[ %]' '{print $5}' | grep -Eo "[0-9]+")
-    [ ! "$(acpi -b | grep 'Battery 0' | grep Discharging)" ] && charge_icon=" "
+    [ -n "$(acpi -a | grep on-line)" ] && charge_icon=" "
     _time="可用时间: $(acpi | sed 's/^Battery 0: //g' | awk -F ',' '{print $3}' | sed 's/^[ ]//g' | awk '{print $1}')"
     [ "$_time" = "可用时间: " ] && _time=""
 }
 
 get_by_upower() {
+    [ -n "$bat_text" ] && [ $bat_text -gt 0 ] && return
     [ ! "$(command -v upower)" ] && echo command not found: upower && return
     bat=$(upower -e | grep BAT)
     bat_text=$(upower -i $bat | awk '/percentage/ {print $2}' | grep -Eo '[0-9]+')
-    charge_icon=" "
-    [ -z "$(upower -i $bat | grep 'state:.*fully-charged')" ] && charge_icon=" "
+    # charge_icon=" "
+    [ -n "$(upower -i $bat | grep 'state:.*fully-charged')" ] && charge_icon=" "
 }
 
 update() {
