abcf40ac38ea4e6e4a93781e8b21c358fd145e6e
chenyc <flyforest@outlook.com>
2023-04-07 19:20:21 +0800

    添加DEF/翻译脚本

---
+++ DEF/translate.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# 利用chatgpt翻译选中的文本
+# https://github.com/j178/chatgpt
+
+msgid=$((RANDOM%1000))
+
+notify-send -r $msgid "󰊿 AI translator" "."; sleep 0.3
+notify-send -r $msgid "󰊿 AI translator" ".."; sleep 0.3
+notify-send -r $msgid "󰊿 AI translator" "...";
+
+r=""
+chatgpt -p translator -n -d "$(xsel -o)" | while IFS= read -r -n1 char
+do
+    r="$r$char"
+    notify-send -r $msgid "󰊿 AI translator" "$r"
+done
