f3204e09149ed3f0f7b248403e9699a898e53865
chenyc <flyforest@outlook.com>
2023-04-10 15:54:08 +0800

    翻译脚本内source环境变量

--- DEF/translate.sh
+++ DEF/translate.sh
@@ -2,6 +2,8 @@
 # 利用chatgpt翻译选中的文本
 # https://github.com/j178/chatgpt
 
+# 载入环境变量 （apiKey, proxyEndpoint）
+source ~/.zshrc
 msgid=$((RANDOM%1000))
 
 notify-send -r $msgid "󰊿 AI translator" "."; sleep 0.3
