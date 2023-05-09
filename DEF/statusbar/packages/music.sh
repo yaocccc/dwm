a8ff19bb16d1abc342d960c42005a2f27541958f
semtor <90167007+semtor@users.noreply.github.com>
2023-05-09 12:38:39 +0800

    Update music.sh

--- DEF/statusbar/packages/music.sh
+++ DEF/statusbar/packages/music.sh
@@ -8,7 +8,7 @@ icon_color="^c#3B102B^^b#6873790x88^"
 text_color="^c#3B102B^^b#6873790x99^"
 signal=$(echo "^s$this^" | sed 's/_//')
 
-[ ! "$(command -v playerctl)" ] && echo command not found: mpc && return
+[ ! "$(command -v playerctl)" ] && echo command not found: playerctl && return
 
 update() {
     music_text="$(playerctl metadata title)"
