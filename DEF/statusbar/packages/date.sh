0c8a49f7c8853230fdf0133324d23c1508c9827a
chenyc <flyforest@outlook.com>
2023-06-14 15:29:12 +0800

    修复date脚本颜色

--- DEF/statusbar/packages/date.sh
+++ DEF/statusbar/packages/date.sh
@@ -33,7 +33,7 @@ update() {
 }
 
 notify() {
-    _cal=$(cal --color=always | sed 1,2d | sed 's/..7m/<b><span color="#ff79c6">/;s/..27m/<\/span><\/b>/' )
+    _cal=$(cal --color=always | sed 1,2d | sed 's/..7m/<b><span color="#ff79c6">/;s/..0m/<\/span><\/b>/')
     _todo=$(cat ~/.todo.md | sed 's/\(- \[x\] \)\(.*\)/<span color="#ff79c6">\1<s>\2<\/s><\/span>/' | sed 's/- \[[ |x]\] //')
     notify-send "  Calendar" "\n$_cal\n————————————————————\n$_todo" -r 9527
 }
