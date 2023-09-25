43872d6c246bbb7b6ee7efd7fdbee5206c7781ed
chenyc <flyforest@outlook.com>
2023-09-25 15:56:45 +0800

    更新bat脚本

--- DEF/statusbar/packages/bat.sh
+++ DEF/statusbar/packages/bat.sh
@@ -13,37 +13,50 @@ get_by_acpi() {
     [ ! "$(command -v acpi)" ] && echo command not found: acpi && return
     bat_text=$(acpi -b | sed '2,$d' | awk '{print $4}' | grep -Eo "[0-9]+")
     [ ! "$bat_text" ] && bat_text=$(acpi -b | sed '2,$d' | awk -F'[ %]' '{print $5}' | grep -Eo "[0-9]+")
-    [ ! "$(acpi -b | grep 'Battery 0' | grep Discharging)" ] && 
-    [ -n "$(acpi -a | grep on-line)" ] && charge_icon=" "
+    [ ! "$(acpi -b | grep 'Battery 0' | grep Discharging)" ] && charging="true"
     _time="可用时间: $(acpi | sed 's/^Battery 0: //g' | awk -F ',' '{print $3}' | sed 's/^[ ]//g' | awk '{print $1}')"
     [ "$_time" = "可用时间: " ] && _time=""
 }
 
 get_by_upower() {
-    [ -n "$bat_text" ] && [ $bat_text -gt 0 ] && return
     [ ! "$(command -v upower)" ] && echo command not found: upower && return
     bat=$(upower -e | grep BAT)
     bat_text=$(upower -i $bat | awk '/percentage/ {print $2}' | grep -Eo '[0-9]+')
-    [ -n "$(upower -i $bat | grep 'state:.*fully-charged')" ] && charge_icon=" "
+    [ "$(upower -i $bat | grep -w charging)" ] && charging="true"
+    [ "$(upower -i $bat | grep 'state:.*fully-charged')" ] && charging=""
 }
 
 update() {
-    get_by_acpi
     get_by_upower
+    get_by_acpi
     [ -z $bat_text ] && bat_text=0
-    if   [ "$bat_text" -ge 95 ]; then bat_icon=""; charge_icon="";
-    elif [ "$bat_text" -ge 90 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 80 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 70 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 60 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 50 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 40 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 30 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 20 ]; then bat_icon="";
-    elif [ "$bat_text" -ge 10 ]; then bat_icon="";
-    else bat_icon=""; fi
+    if [ "$charging" ]; then
+        if   [ "$bat_text" -ge 95 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 90 ]; then bat_icon="󰂋";
+        elif [ "$bat_text" -ge 80 ]; then bat_icon="󰂊";
+        elif [ "$bat_text" -ge 70 ]; then bat_icon="󰢞";
+        elif [ "$bat_text" -ge 60 ]; then bat_icon="󰂉";
+        elif [ "$bat_text" -ge 50 ]; then bat_icon="󰢝";
+        elif [ "$bat_text" -ge 40 ]; then bat_icon="󰂈";
+        elif [ "$bat_text" -ge 30 ]; then bat_icon="󰂇";
+        elif [ "$bat_text" -ge 20 ]; then bat_icon="󰂆";
+        elif [ "$bat_text" -ge 10 ]; then bat_icon="󰢜";
+        else bat_icon="󰢟"; fi
+    else
+        if   [ "$bat_text" -ge 95 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 90 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 80 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 70 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 60 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 50 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 40 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 30 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 20 ]; then bat_icon="";
+        elif [ "$bat_text" -ge 10 ]; then bat_icon="";
+        else bat_icon=""; fi
+    fi
 
-    icon=" $charge_icon$bat_icon "
+    icon=" $bat_icon "
     text=" $bat_text% "
 
     sed -i '/^export '$this'=.*$/d' $tempfile
@@ -52,6 +65,7 @@ update() {
 
 notify() {
     update
+    if [ "$charging" ]; then _time=""; fi
     notify-send "$bat_icon Battery" "\n剩余: $bat_text%\n$_time" -r 9527
 }
 
